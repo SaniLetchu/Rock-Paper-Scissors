72892574710963abc77a1329acbd7fcd63577a55
Sani Letchu <sani.letchu@gmail.com>
2022-05-10 18:53:17 +0300

    Created playRound function that works takes two inputs and returns who won

--- script.js
+++ script.js
@@ -11,4 +11,43 @@ function computerPlay() {
     }
 }   
 
-console.log(computerPlay());
+function playRound(playerSelection, computerSelection) {
+    function win(player, computer) {
+        return `You Win! ${player} beats ${computer}`;
+    }
+    function lose(player, computer) {
+        return `You Lose! ${computer} beats ${player}`;
+    }
+    let playerchoice = playerSelection.toLowerCase().split(' ').map((s) => s.charAt(0).toUpperCase() + s.substring(1)).join(' ');
+    if(playerchoice == computerSelection) {
+        return "Draw!";
+    }
+    if(playerchoice == "Rock") {
+        if(computerSelection == "Scissors") {
+            return win(playerchoice, computerSelection);
+        }
+        else {
+            return lose(playerchoice, computerSelection);
+        }
+    }
+    else if(playerchoice == "Scissors") {
+        if(computerSelection == "Paper") {
+            return win(playerchoice, computerSelection);
+        }
+        else {
+            return lose(playerchoice, computerSelection);
+        }
+    }
+    else if(playerchoice == "Paper") {
+        if(computerSelection == "Rock") {
+            return win(playerchoice, computerSelection);
+        }
+        else {
+            return lose(playerchoice, computerSelection);
+        }
+    }
+}
+
+const playerSelection = "rock";
+const computerSelection = computerPlay();
+console.log(playRound(playerSelection, computerSelection));
